2aaca309daba4f5e3254885e840cc769835a1cc1
SeifeYonas <syonas7777@gmail.com>
2020-08-12 20:45:42 -0700

    This commit includes setting up Google Firebase with the backend

--- api/routes/testAPI.js
+++ api/routes/testAPI.js
@@ -1,7 +1,20 @@
 var express = require("express");
 var router = express.Router();
+var admin = require("firebase-admin");
+
+var serviceAccount = require("../serviceAccount.json");
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://fycourse-b4f8c.firebaseio.com/",
+});
 
 router.get("/", function (req, res, next) {
+  const db = admin.firestore();
+  db.collection("sampleCollection")
+    .doc("sampleDocument")
+    .set({ Name: "Seife Yonas", LevelOfFyness: "900" })
+    .then(() => console.log("my literal response"))
+    .catch(() => console.log("error"));
   res.send("API is working");
 });
 
